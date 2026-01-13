8b27e6067c101f32214d4952bfda1e4e685554ee
Camila Paschini <camila.paschini@gmail.com>
2026-01-13 08:20:04 +0100

    Add project files.

---
+++ Gymbooking/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Gymbooking.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
